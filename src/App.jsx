e4c69fed27b54a958b2ec3a6a0654394c38afdd6
Nekit <nick.misiura@yandex.ru>
2024-10-18 17:01:32 +0300

    feat: add volume slider; ref: fix css styles, add comments

--- src/App.jsx
+++ src/App.jsx
@@ -13,17 +13,27 @@ const SOUNDMAP_1 = {
   C: 'https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3',
 };
 
-function SoundButton({keyButton, url, keyDownMap, display}) {
-  const refAudio = useRef();
+function SoundButton({keyButton, url, keyDownMap, display, volume}) {
   const refButton = useRef();
+  const refAudio = useRef();
+  // const sound = new Audio(url);
 
-  keyDownMap[keyButton] = refButton;
+  // sound.volume = volume / 100;
+
+  //создается ссылка на <button> с ключем нажимаемой кнопки (Q, W, E, etc)
+  keyDownMap[keyButton] = refButton; 
 
   const handleClick = (_event) => {
     refAudio.current.currentTime = 0;
     refAudio.current.play();
     display.current.innerText = `${keyButton}-sound`;
   };
+  
+  useEffect(() => {
+    if (refAudio.current?.volume || refAudio.current?.volume === 0) {
+      refAudio.current.volume = (volume || 1) / 100;
+    }
+  },[volume]);
   
   return (
     <button  ref={refButton} id={keyButton} className='drum-pad' onClick={handleClick} >
@@ -33,24 +43,45 @@ function SoundButton({keyButton, url, keyDownMap, display}) {
   );
 }
 
+function VolumeControl({volume, setVolume}) {
+  const handleChange = (event) => {
+    setVolume(event.target.value);
+  };
+  
+  return (
+    <div id="volume-control-box">
+      <label htmlFor='volume-control'>Громкость:<span> {volume}%</span></label>
+      <input id="volume-control" type='range' value={volume} onChange={handleChange}/>
+    </div>
+  );
+}
+
 function App() {
-  const soundButtons = new Array();
-  const refDisplay = useRef();
+  const [volume, setVolume] = useState(50);   //изначальная громкость
+  const refDisplay = useRef();                //реф на div элемент, отображает название трека, передается в SoundButton
 
-  const keyDownMap = {};
+  //лист с компонентами, содержащими кнопки
+  const soundButtons = new Array();               
+
+  //объект для ссылок на кнопки, передается в каждый компонент SoundButton
+  const keyDownMap = {};                          
+  
+  //собрать ключи и url для каждого трека, создать компонент и передать параметры
   for (const keyButton in SOUNDMAP_1) {
-    soundButtons.push(<SoundButton display={refDisplay} keyDownMap={keyDownMap} key={`${keyButton}-sound`} id={`${keyButton}-sound`} keyButton={keyButton} url={SOUNDMAP_1[keyButton]} />);
-
+    soundButtons.push(<SoundButton  key={`${keyButton}-sound`} id={`${keyButton}-sound`} display={refDisplay} keyDownMap={keyDownMap} keyButton={keyButton} url={SOUNDMAP_1[keyButton]} volume={volume} />);
   }
 
+  //обработать нажатие кнопки
+  //если кнопка совпадает с одним из ключей в SOUNDMAP_1, то кликается на соотв кнопка
   const handleKeyDown = (event) => {
     const key = event.key.toUpperCase();
     console.log(key);
     if (Object.keys(SOUNDMAP_1).includes(key)) {
-      keyDownMap[key].current.click();
+      keyDownMap[key].current.click(); //нажимает на нужный <button>
     }
   };
 
+  //после рендера удаляется старый и создается новый ивент на нажатие кнопки
   useEffect(() => {
     window.removeEventListener('keydown', handleKeyDown); //prevents adding countless events
     window.addEventListener('keydown', handleKeyDown);
@@ -61,8 +92,8 @@ function App() {
       <div id="drum-machine">
         {soundButtons}
       </div>
-      <div id="display" ref={refDisplay} >
-      </div>
+      <VolumeControl volume={volume} setVolume={setVolume} />
+      <div id="display" ref={refDisplay}></div>
     </>
   );
 }
